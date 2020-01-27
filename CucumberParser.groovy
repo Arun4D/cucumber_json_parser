fd2c18c3db1f74f9c91eba013acfbe17b4136b3e
Arun4D <arun4.duraisamy@gmail.com>
2020-01-27 21:01:31 +0200

    Update CucumberParser.groovy

--- CucumberParser.groovy
+++ CucumberParser.groovy
@@ -79,8 +79,6 @@ cucumberResultObj.each { arrayValue ->
                                                         }
                                                 }
                                             }
-
-
                                     }
                             }
                         }
